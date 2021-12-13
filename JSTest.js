0ff0e3bdafe3eb4656ccb4b13d931d1ff7acb711
Xuelun Du <woshidududu@qq.com>
2021-12-13 22:22:40 +0800

    add a test for JavaScript

---
+++ JSTest.js
@@ -0,0 +1,13 @@
+PrintLine('Start')
+
+let a = "abcd"
+for(let i = 0; i < a.length; i++) {
+    console.log(a[i])
+}
+
+PrintLine('End')
+
+function PrintLine(s) {
+    console.log('------' + s + '------')
+}
+
