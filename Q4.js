faa6d4f042e02648548a155bcc83359d8a448404
Xuelun Du <woshidududu@qq.com>
2021-12-13 21:58:07 +0800

    Q4 test added

---
+++ Q4.js
@@ -0,0 +1,19 @@
+var findMedianSortedArrays = function(nums1, nums2) {
+    let l1 = nums1.length -1, l2 = nums2.length - 1
+    if(l1 < l2) return findMedianSortedArrays(nums2, nums1)
+
+    let p = Math.floor((l1 + l2 + 1) / 2)
+    let p1 = 0, p2 = 0
+
+    const loopLimit = 50
+    let loop = 0
+    while(midNo > 1) {
+        let p = Math.floor(midNo / 2)
+        if(nums1[p1 + p] >= nums2[p2 + p]) p2 = p2 + p
+        else p1 = p1 + p
+
+        //loop control
+        loop++
+        if(loop > loopLimit) break
+    }
+};
